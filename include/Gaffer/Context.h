725093158f7a96261f03b380515893216c1b7752
Eric Mehl <eric@thinkhypothetical.com>
2021-06-16 13:46:34 -0400

    Context : add symbol exports

--- include/Gaffer/Context.h
+++ include/Gaffer/Context.h
@@ -307,7 +307,7 @@ class GAFFER_API Context : public IECore::RefCounted
 		// along with the `IECore::TypeId` for `TypedData<T>`, which is used to
 		// validate type-safe access. Does not manage memory or ownership in any
 		// way : this is the responsibility of calling code.
-		struct Value
+		struct GAFFER_API Value
 		{
 
 			Value();
@@ -349,7 +349,7 @@ class GAFFER_API Context : public IECore::RefCounted
 				const void *m_value;
 				IECore::MurmurHash m_hash;
 
-				struct TypeFunctions
+				struct GAFFER_API TypeFunctions
 				{
 					IECore::DataPtr (*makeData)( const Value &value, const void **dataValue );
 					bool (*isEqual)( const Value &a, const Value &b );
