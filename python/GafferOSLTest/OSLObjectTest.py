fce25f7db8740969cf8b050370d97eab2f5581f4
Eric Mehl <eric@thinkhypothetical.com>
2022-09-12 10:14:25 -0400

    OSLTest : Fix `subprocess` import on Windows

--- python/GafferOSLTest/OSLObjectTest.py
+++ python/GafferOSLTest/OSLObjectTest.py
@@ -34,9 +34,13 @@
 #
 ##########################################################################
 
-import os
 import imath
-import subprocess
+import os
+import sys
+if os.name == 'posix' and sys.version_info[0] < 3:
+	import subprocess32 as subprocess
+else:
+	import subprocess
 import inspect
 
 import IECore
