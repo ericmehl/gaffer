661dde05dada07cbf318836430a8bc4f48cdfdb6
Eric Mehl <eric@thinkhypothetical.com>
2024-10-04 15:44:13 -0400

    WidgetEditor : Add width and height columns

--- python/GafferUI/WidgetEditor.py
+++ python/GafferUI/WidgetEditor.py
@@ -103,6 +103,8 @@ class WidgetPath( Gaffer.Path ) :
 			"widgetEditor:name",
 			"widgetEditor:widget",
 			"widgetEditor:widgetType",
+			"widgetEditor:width",
+			"widgetEditor:height",
 		]
 
 	def property( self, name, canceller = None ) :
@@ -122,6 +124,10 @@ class WidgetPath( Gaffer.Path ) :
 			return widget
 		elif name == "widgetEditor:widgetType" :
 			return type( widget ).__name__
+		elif name == "widgetEditor:width" :
+			return widget._qtWidget().size().width()
+		elif name == "widgetEditor:height" :
+			return widget._qtWidget().size().height()
 
 	def widget( self ) :
 		# Returns the `GafferUI.Widget` for this path.
@@ -210,6 +216,8 @@ class WidgetEditor( GafferUI.Editor ) :
 				columns = (
 					self.__widgetNameColumn,
 					GafferUI.PathListingWidget.StandardColumn( "Type", "widgetEditor:widgetType" ),
+					GafferUI.PathListingWidget.StandardColumn( "Width", "widgetEditor:width" ),
+					GafferUI.PathListingWidget.StandardColumn( "Height", "widgetEditor:height" ),
 				),
 				selectionMode = GafferUI.PathListingWidget.SelectionMode.Row,
 				displayMode = GafferUI.PathListingWidget.DisplayMode.Tree
