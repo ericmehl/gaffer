7eb1d39e5808db83af4c0246871106ad95d0e8b3
Eric Mehl <eric@thinkhypothetical.com>
2026-09-11 15:31:57 -0400

    RenderTest : Add USDMeshLight test

--- python/GafferArnoldTest/ArnoldRenderTest.py
+++ python/GafferArnoldTest/ArnoldRenderTest.py
@@ -1770,6 +1770,11 @@ class ArnoldRenderTest( GafferSceneTest.RenderTest ) :
 			with GafferTest.TestRunner.PerformanceScope() :
 				s["render"]["task"].execute()
 
+	@unittest.skip( "No USDMeshLight support just yet" )
+	def testUSDMeshLight( self ) :
+
+		pass
+
 	def _createConstantShader( self ) :
 
 		shader = GafferArnold.ArnoldShader()
@@ -1782,6 +1787,12 @@ class ArnoldRenderTest( GafferSceneTest.RenderTest ) :
 		shader.loadShader( "lambert" )
 		shader["parameters"]["Kd"].setValue( 1 )
 		return shader, shader["parameters"]["Kd_color"], shader["out"]
+
+	def _createEmissiveShader( self ) :
+
+		shader = GafferArnold.ArnoldShader()
+		shader.loadShader( "flat" )
+		return shader, shader["parameters"]["color"], shader["out"]
 
 	def _createPointLight( self ) :
 
